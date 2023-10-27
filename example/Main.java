7cb0b2bcb20ba1cff034706a680ffa1e9639edaf
unknown <abinashappu9@gmail.com>
2023-10-26 21:13:57 -0700

    added chennal name

--- example/Main.java
+++ example/Main.java
@@ -4,21 +4,21 @@ import java.sql.*;
 import static org.example.Constants.*;
 
 public class Main {
-//    public void addProduct(int product_ID, String name, int price, int quantity) {
-//        try (Connection conn = DriverManager.getConnection(url, user, password)) {
-//            String insertQuery = "INSERT INTO practice.product (product_ID, name, price, quantity) VALUES (?,?, ?, ?)";
-//            try (PreparedStatement preparedStatement = conn.prepareStatement(insertQuery)) {
-//                preparedStatement.setInt(1, product_ID);
-//                preparedStatement.setString(2, name);
-//                preparedStatement.setInt(3, price);
-//                preparedStatement.setInt(4, quantity);
-//                preparedStatement.executeUpdate();
-//            }
-//        } catch (SQLException e) {
+    public void addProduct(int product_ID, String name, int price, int quantity) {
+        try (Connection conn = DriverManager.getConnection(url, user, password)) {
+            String insertQuery = "INSERT INTO practice.product (product_ID, name, price, quantity) VALUES (?,?, ?, ?)";
+            try (PreparedStatement preparedStatement = conn.prepareStatement(insertQuery)) {
+                preparedStatement.setInt(1, product_ID);
+                preparedStatement.setString(2, name);
+                preparedStatement.setInt(3, price);
+                preparedStatement.setInt(4, quantity);
+                preparedStatement.executeUpdate();
+            }
+        } catch (SQLException e) {
 
-//            e.printStackTrace();
-//        }
-//    }
+            e.printStackTrace();
+        }
+    }
     public void viewProducts(){
         try (Connection connection = DriverManager.getConnection(url, user, password)) {
             String SelectQuery = "Select * from practice.product";
@@ -39,10 +39,11 @@ while(resultSet.next()) {
     }
 
 
+
     public static void main(String[] args) {
         Main main = new Main();
 //        main.addProduct(5, "laptop",25000,50 );
         main.viewProducts();
-        System.out.println("add succesfully");
+//        System.out.println("add succesfully");
 }
 }
